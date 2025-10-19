f9f72e11e03b5bda89fb5d9d454c22cbd4dad794
Krishnandu Das <daskrishnandu577@gmail.com>
2025-10-19 12:30:18 +0530

    Add files via upload

---
+++ 1.1_Factorial_Recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1; // Base case: 0! or 1! = 1
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << num << " is: " << factorial(num) << endl;
+    }
+
+    return 0;
+}
